2c7e573ec3b949cf21027b15cc15ac521c173b05
yhl1999 <yhl9933@foxmail.com>
2019-12-01 15:38:23 +0800

    readme补充、增加服务器接口和测试脚本

---
+++ Server/api.py
@@ -0,0 +1,105 @@
+import pymysql
+import time
+import datetime
+from sqlalchemy import *
+from sqlalchemy.orm import create_session
+from sqlalchemy.ext.declarative import declarative_base
+from configs import DB_URI
+
+engine = create_engine(DB_URI)
+Base = declarative_base()
+metadata = MetaData(bind=engine)
+session = create_session(bind = engine)
+
+class User(Base):
+    __table__ = Table('user',metadata,autoload = True)
+
+    
+    #构造函数
+    def __init__(self,account,password,username,rdate,headpic,coin,role):
+        self.account = account
+        self.password = password
+        self.username = username
+        self.rdate = rdate
+        self.headpic = headpic
+        self.coin = coin
+        self.role = role
+
+
+    #账号存在性检验
+    def ifAcntExist(userAcnt):
+        acnt = session.query(User).filter(User.account==userAcnt).all()
+        l = len(acnt)
+        if l == 1 :
+            return acnt[0]
+        else :
+            return l
+
+    #创建新用户 测试完毕
+    def addUser(newAcnt,newPwd):
+        if User.ifAcntExist(newAcnt) !=0 :
+            print("账号存在")
+            return -1 #账号已存在
+        else:
+            nowdate = datetime.datetime.now()
+            nickname = newAcnt
+            headpic = ""
+            coin = 0
+            role = 1
+
+            newuser = User(newAcnt,newPwd,nickname,nowdate,headpic,coin,role)
+            session.begin()
+            session.add(newuser)    
+            if session.commit() == None :
+                print("账号创建成功")
+                return 1 #账号创建成功
+            else:
+                print("账号创建失败")
+                return 0 #账号创建失败
+
+    #修改金币数 测试完毕
+    def changeCoin(userAcnt,code,num):
+        e = User.ifAcntExist(userAcnt)
+        
+        if isinstance(e,int):
+            if e>1:
+                print("数据错误！存在多个相同账号！")
+                return False
+            elif e==0 :
+                print("账号不存在")
+                return False
+        else :           
+            if code == 1:
+                e.coin +=num
+            elif code == 0:
+                e.coin -=num
+            else:
+                print("操作码错误")
+                return False
+            session.begin()
+            session.commit()
+            return True
+            
+    
+    #修改密码 测试完毕
+    def changePwd(userAcnt,oldPwd,newPwd):
+        e = User.ifAcntExist(userAcnt)
+
+        if isinstance(e,int):
+            if e>1:
+                print("数据错误！存在多个相同账号！")
+                return 3
+            elif e==0 :
+                print("账号不存在")
+                return 2
+        else:
+            if e.password == oldPwd :
+                e.password = newPwd
+                return 0
+            else:
+                print("原密码错误")
+                return 1
+    
+
+
+
---
+++ Server/test.py
@@ -0,0 +1,67 @@
+import api
+
+session = api.session
+User = api.User
+
+def show(rest):
+    for i in rest:
+        print(i.account +" "+i.password+" "+str(i.coin))
+    print ('-'*15)
+
+#addUser测试
+def tst_addUser():
+    act = "tst_adduser2.0"
+    pwd = "123456"
+    rest = session.query(User).all()
+    show(rest)
+
+    User.addUser(act,pwd)
+
+    rest = session.query(User).all()
+    show(rest)
+
+
+
+
+#changeCoin测试
+def tst_changeCoin():
+    act = "tst_changeCoin2.4"
+    pwd = "123456"
+    rest = session.query(User).all()
+    show(rest)
+
+    User.addUser(act,pwd)
+    #增加金币
+    User.changeCoin(act,1,100)
+
+    rest = session.query(User).all()
+    show(rest)
+    #减少金币
+    User.changeCoin(act,0,50)
+
+    rest = session.query(User).all()
+    show(rest)
+
+#changePwd测试
+def tst_changePwd():
+    act = "tst_changePwd1.0"
+    pwd = "123456"
+    npwd = "abcde"
+    User.addUser(act,pwd)
+    rest = session.query(User).all()
+    show(rest)
+    
+    User.changePwd(act+"s",pwd,npwd)
+    rest = session.query(User).all()
+    show(rest)
+
+    User.changePwd(act,pwd,npwd)
+    rest = session.query(User).all()
+    show(rest)
+
+    User.changePwd(act,pwd,npwd)
+    rest = session.query(User).all()
+    show(rest)
+#tst_addUser()
+#tst_changeCoin()
+tst_changePwd()
